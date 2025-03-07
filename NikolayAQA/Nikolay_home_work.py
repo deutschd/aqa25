3ccc740bc544ee9a06e068509f0b1176c9055982
Nikolay Garipov <kolya.garipov.2000@mail.ru>
2025-03-07 22:27:05 +0300

    домашка

---
+++ NikolayAQA/Nikolay_home_work.py
@@ -0,0 +1,14 @@
+#2.1 Переменные
+
+#t = "Hello world!"
+#print(t)
+
+
+#2.2 Оператор ввода input
+
+#name = input("Введите имя ")
+#print("Привет")
+#print(name)
+#name2 = input("Введите второе имя ")
+#print("Привет, ")
+#print(name2)
