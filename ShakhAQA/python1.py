65c0c7c3785a4c7b846eed090f46d9e63d3d90aa
deutschd <ismailovshakhzod@gmail.com>
2025-03-08 19:05:46 +0500

    Recall - Input

--- ShakhAQA/python1.py
+++ ShakhAQA/python1.py
@@ -1,1 +1,19 @@
 print("helloworld")
+
+# BASIC
+# Variables
+
+text = '\nPython!'
+print(text*5)
+
+
+# Input
+
+# text_input = input("What text to Display?: ")
+# print("There's your text: " + text_input)
+# text_input2 = input("What will be the second name?: ")
+# print("There's the second text: " + text_input2)
+
+
+
+
