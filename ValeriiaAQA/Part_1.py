2ec00b1ff6e85cc08969d1d2a61d631f7616b34b
Fallen Hero <failent.hero@gmail.com>
2025-03-16 15:36:22 +0100

    Cycle while

--- ValeriiaAQA/Part_1.py
+++ ValeriiaAQA/Part_1.py
@@ -24,4 +24,39 @@
 # print((int(n) % 100) + (int(m) % 100))
 
 #------------------------------------------------------------------#
-# Topic 5
+# Topic 6. Operator while
+
+# 6.3 Task 1
+# temperature = int(input("Enter the temperature: "))
+# distance = 0
+#
+# while temperature > 15:
+#     distance += 20
+#     temperature -= 2
+#     if temperature <= 15:
+#         break
+#     distance += 10
+#
+# print("Спортсмен прошел", distance, "метров.")
+
+#------------------------------------------------------------------#
+# Topic 7. Cycle for
+
+# 7.6 Task 7
+
+# First variant without any checks
+# amount_of_cards = int(input("Please, enter the amount of cards: "))
+# total_cards = sum(range(1, amount_of_cards + 1))
+#
+# for i in range(amount_of_cards - 1):
+#     total_cards -= int(input("Enter the number of the remaining card: "))
+# print(f" The missing card is: {total_cards}")
+
+
+
+
+
+
+
+
+
