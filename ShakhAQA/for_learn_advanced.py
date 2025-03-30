e167c13e007d4953fe112cc6a6288d1cc94470fb
deutschd <ismailovshakhzod@gmail.com>
2025-03-30 15:26:37 +0500

    Recall - for advanced - string beginning

--- ShakhAQA/for_learn_advanced.py
+++ ShakhAQA/for_learn_advanced.py
@@ -1,1 +1,40 @@
-print('\tJOB WITH STRING')
+print('\tSTRING')
+print('\tHOMEWORK from 8.0 GIRLS AND BOYS in THEATRE')
+boys = 3 # int(input('Enter number of boys: '))
+girls = 6 # int(input('Enter number of girls: '))
+answer = ''
+if (boys > 2 * girls) or (girls > 2 * boys):
+    print('no answer')
+elif boys >= girls:
+    k = boys - girls
+    for bgb in range(k):
+        answer += 'BGB'
+    for bg in range(girls - k):
+        answer += 'BG'
+else:
+    k = girls - boys
+    for gbg in range(k):
+        answer += 'GBG'
+    for gb in range(boys - k):
+        answer += 'GBG'
+print(answer)
+
+print('\n\tCHECK PASSWORD')
+# password = input('Enter password: ')
+# while password != 'Python235':
+#     print('Incorrect pass!')
+#     password = input('Try again!: ')
+# print('Correct password!')
+
+print('\n\tLITERATURE SCORE IN CLASS')
+# badGradeCount = 0
+# for student in range(5):
+#     answer = input('Who wrote this text?: ')
+#     if answer == 'Pushkin' or answer == 'pushkin':
+#         print('Correct!')
+#         break
+#     print('It is wrong...')
+#     badGradeCount += 1
+# print('Number of 2: ', badGradeCount)
+
+print('\n\tPRACTICE - ')
