0fa2ac981b62523cea81ef90d692a666d79d8c0d
deutschd <ismailovshakhzod@gmail.com>
2025-04-04 23:03:28 +0500

    Recall - Nested loops (10.1 practice)

--- ShakhAQA/nested_loops.py
+++ ShakhAQA/nested_loops.py
@@ -17,9 +17,26 @@ for row in range(0, 6, 2):
         print(row + col, end =' ')
     print()
 
+for row in range(1, 10):
+    for col in range(1, 10):
+        print(row * col, end='\t')
+    print()
 
+number = 6 # int(input('Enter number: '))
+for row in range(number):
+    for col in range(number):
+        print(row + col, end = ' ')
+    print()
 
+for row in range(9):
+    for col in range(1, -9, -1):
+        print(row + col, end = '\t')
+    print()
 
-
-
-
+for row in range(1, 5, 1):
+    for col in range(1, 5):
+        if col % 2 == 0:
+            print(col, end=' ')
+        if row % 2 == 0:
+            print(row, end=' ')
+    print()
