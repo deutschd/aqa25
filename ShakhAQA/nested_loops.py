5aab36f1b0ff05186aeb2d9c771f969b458a5fe7
deutschd <ismailovshakhzod@gmail.com>
2025-04-10 23:40:57 +0500

    Recall - If inside Nested loops (practice)

--- ShakhAQA/nested_loops.py
+++ ShakhAQA/nested_loops.py
@@ -85,5 +85,14 @@ for row in range(1, size + 1, 1):
             print(row, end=' ')
     print()
 
+for row in range(20):
+    for col in range(50):
+        if row == 9:
+            print('-', end='')
+        elif col == 25:
+            print('|', end='')
+        else:
+            print(' ', end='')
+    print()
 
 
