c3a0f50270272005c8a2bbd23731f0c474d65ff8
deutschd <ismailovshakhzod@gmail.com>
2025-04-13 23:16:50 +0400

    Two counters in if operators (BUILDING A ROAD)

--- ShakhAQA/nested_loops.py
+++ ShakhAQA/nested_loops.py
@@ -119,3 +119,30 @@ for row in range(20):
         else:
             print(' ', end='')
     print()
+
+print('\n\tPRACTICE - GATES')
+for row in range(20):
+    for col in range(30):
+        if row == 0:
+            print('-', end='')
+        elif col == 0 or col == 29:
+            print('|', end='')
+        else:
+            print(' ', end='')
+    print()
+
+print('\n\tPRACTICE - GATES')
+for row in range(20):
+    for col in range(50):
+        if row == 9:
+            print('-', end='')
+        elif col == -row + 19:
+            print('/', end='')
+        elif col == row +29:
+            print("\\", end='')
+        elif col == 24:
+            print('|', end='')
+        else:
+            print(' ', end='')
+    print()
+
