caaa5c4787f6163bc961fc6fea2e9ede712e060b
Nikolay Garipov <kolya.garipov.2000@mail.ru>
2025-05-21 22:05:06 +0300

    копаем яму

--- NikolayAQA/home_work_2.py
+++ NikolayAQA/home_work_2.py
@@ -314,6 +314,20 @@
 #         current += 2
 #     print()
 
+# `````````````````Дз```````````````````````#
+
+# n = int(input("Введите глубину ямы:"))
+#
+# for row in range(1, n + 1):
+#     for col in range(n, n - row, -1):
+#         print(col, end="")
+#     print("." * (2 * (n - row)), end="")
+#
+#     for col in range(n - row + 1, n + 1):
+#         print(col, end="")
+#     print()
+
+
 
 
 
