cefa7f1aed61ae3296b108df7ec3356e36f8b383
Nikolay Garipov <kolya.garipov.2000@mail.ru>
2025-05-22 21:45:03 +0300

    разбор дз

--- NikolayAQA/home_work_2.py
+++ NikolayAQA/home_work_2.py
@@ -327,6 +327,51 @@
 #         print(col, end="")
 #     print()
 
+# `````````````````Дз```````````````````````#
+
+# rows = int(input("Кол-во уровней?:"))
+# new_num = 1
+#
+# for line in range(rows):
+#
+#     for space in range(rows - line - 1, 0, -1):
+#         print(end=' ')
+#
+#     for number in range(line + 1):
+#         print(new_num, end=' ')
+#         new_num += 2
+#     print()
+
+# `````````````````Дз```````````````````````#
+
+# rows = int(input("Кол-во уровней?:"))
+# new_num = 1
+#
+# for line in range(rows):
+#     space_count = rows - line - 1
+#     print(' ' * space_count, end='')
+#     for number in range(line + 1):
+#         print(new_num, end=' ')
+#         new_num += 2
+#     print()
+
+# `````````````````Дз```````````````````````#
+
+# depth = int(input("Кол-во уровней?:"))
+#
+# for line in range(depth):
+#
+#     for left_number in range(depth, depth - line, -1):
+#         print(left_number, end='')
+#
+#     point_count = 2 * (depth - line - 1)
+#     print('.' * point_count, end='')
+#
+#     for right_number in range(depth - line, depth + 1):
+#         print(right_number, end='')
+#
+#     print()
+
 
 
 
