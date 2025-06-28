02d1d974387b6a3d5f0fb22e5c9b423577d98566
Nikolay Garipov <kolya.garipov.2000@mail.ru>
2025-06-28 16:14:33 +0300

    библиотека decimal

---
+++ NikolayAQA/practice_2.py
@@ -0,0 +1,28 @@
+#13.4 Научное представление и точная работа с вещественными числами
+
+# ant_weight = 2e-6
+# print(f'The weight of ant {ant_weight: .6f}kg')
+
+# +++++++++++++++++++++++++++++++++++++++++++#
+
+# earth_weight = 5.97e+24
+# print(f'The weight of Eart {earth_weight}')
+
+# +++++++++++++++++++++++++++++++++++++++++++#
+
+# import decimal
+#
+# print(decimal('1.1') + decimal('4.4'))
+
+# +++++++++++++++++++++++++++++++++++++++++++#
+
+# def devide_n_times(value, devider, time):
+#     value = decimal(value)
+#     for i in range(time):
+#         value /= devider
+#
+#     return value
+#
+# devide_n_times()
+
+# +++++++++++++++++++++++++++++++++++++++++++#
