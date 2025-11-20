3d59cafaf431ee628eb9724e8d1534131751fc3d
deutschd <ismailovshakhzod@gmail.com>
2025-11-20 17:50:48 +0500

    Revert "examine (chess 2 squares of a chessboard match) - check"
    This reverts commit 7c4a1ad93a8b3eb64c3ca625361980883a139fae.

--- ShakhAQA/stepik_inception/basic.py
+++ ShakhAQA/stepik_inception/basic.py
@@ -101,5 +101,63 @@ print('Ход королём:')
 # if abs(a - c) <= 1 and abs(b - d) <= 1:
 #     print("YES")
 # else:
-#     print("NO")d
+#     print("NO")
+print('\n2 squares of a chessboard match')
+a = 4 # int(input())
+b = 4 # int(input())
+c = 5 # int(input())
+d = 5 # int(input())
+if a==c and b==d:
+    print('YES')
+elif (a+b)%2==0 and (c+d)%2==0:
+    print('YES')
+elif (a+b)%2!=0 and (c+d)%2!=0:
+    print('YES')
+else:
+    print('NO')
 
+
+a = 4 # int(input())
+b = 4 # int(input())
+c = 5 # int(input())
+d = 5 # int(input())
+if a-b==c-d:
+    print('YES')
+elif a+b==c+d:
+    print('YES')
+else:
+    print('NO')
+
+# a = int(input())
+# b = int(input())
+# c = int(input())
+# d = int(input())
+# if a==(c+2) and b==(d-1):
+#     print('YES')
+# elif a==(c+1) and b==(d-2):
+#     print('YES')
+# elif a==(c-1) and b==(d-2):
+#     print('YES')
+# elif a==(c-2) and b==(d-1):
+#     print('YES')
+# elif a==(c-2) and b==(d+1):
+#     print('YES')
+# elif a==(c-1) and b==(d-2):
+#     print('YES')
+# elif a==(c+1) and b==(d+2):
+#     print('YES')
+# elif a==(c+2) and b==(d+1):
+#     print('YES')
+# elif a==(c-1) and b==(d+2):
+#     print('YES')
+# else:
+#     print('NO')
+a=354
+print(a//100, a//10%10, a%10)
+a=int(input())
+c= max(a//100, a//10%10, a%10) - min(a//100, a//10%10, a%10)
+d= (a//100 + a//10%10 + a%10) - (max(a//100, a//10%10, a%10) + min(a//100, a//10%10, a%10))
+if c == d:
+    print('Число интересное')
+else:
+    print('Число неинтересное')
