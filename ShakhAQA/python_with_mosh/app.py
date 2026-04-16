430c9f8c15fb87bf427fffc9c3651b33a4297734
deutschd <ismailovshakhzod@gmail.com>
2026-04-16 16:27:48 +0500

    123

--- ShakhAQA/python_with_mosh/app.py
+++ ShakhAQA/python_with_mosh/app.py
@@ -1,3 +1,28 @@
-x = 1
-y = 2
-unit_price = 3
+course_name = "Python Programming"
+print(len(course_name))        # 18
+print(course_name[0])          # P
+print(course_name[-1])         # g
+print(course_name[0:3])        # Pyt
+print(course_name[0:])         # Python Programming
+print(course_name[:3])          # Python Programming
+print(course_name[:])           # Python Programming
+
+
+# string methods
+course = "  Python \"Programming\""
+print(course.upper())          # PYTHON "PROGRAMMING"
+print(course.lower())          # python "programming"
+print(course.title())          # Python "Programming"
+print(course.strip())          # Python "Programming"
+print(course.find("gram"))       # 13
+print(course.replace("P", "J"))  # Jython "Jrogramming"
+print("gram" in course)          # True
+print("John" not in course)          # True
+print('original string:', course)
+
+# contatination
+first = "John"
+last = "Smith"
+full_name = f"{len(first)} {last} {2 + 2}"
+print(full_name)              # 4 Smith 4
+das
