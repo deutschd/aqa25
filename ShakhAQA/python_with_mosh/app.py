2e0419280505dfa2a1a25c0ec2ac0eb2c626e466
deutschd <ismailovshakhzod@gmail.com>
2026-04-16 15:10:48 +0500

    da

--- ShakhAQA/python_with_mosh/app.py
+++ ShakhAQA/python_with_mosh/app.py
@@ -1,3 +1,8 @@
-x = 1
-y = 2
-unit_price = 3
+"""Module for student management."""
+course_name = "Python Programming"
+
+print(len(course_name))        # 18
+print(course_name[0])          # P
+print(course_name[-1])         # g
+print(course_name[0:3])        # Pyt
+print(course_name[0:8])         # Python Programming
