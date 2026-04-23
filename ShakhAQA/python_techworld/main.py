f58b9186d3f296caec67f2e389875cd89d0516d6
deutschd <ismailovshakhzod@gmail.com>
2026-04-23 16:21:26 +0500

    TechWorld with Nana - nested if else

--- ShakhAQA/python_techworld/main.py
+++ ShakhAQA/python_techworld/main.py
@@ -5,14 +5,14 @@ print('72%7 =', 72%7)
 
 print(f"10 days are {10 * 24 * 60} minutes")
 
-# Variables
+# variables
 to_seconds = 24 * 60 * 60
 name_of_unit = "seconds"
 print(f"10 days are {10 * to_seconds} {name_of_unit}")
 print(f"30 days are {30 * to_seconds} seconds")
 print(f"20 days are {20 * to_seconds} seconds")
 
-# Functions + their parameters
+# functions + their parameters
 def days_to_units(num_of_days, custom_message):
     print(f"{num_of_days} days are {num_of_days * to_seconds} {name_of_unit}")
     print(custom_message)
@@ -27,3 +27,54 @@ def scope_check(num_of_days):
     print(my_var)
 
 scope_check(21)
+
+print('\naccepting user input')
+# def days_to_units(num_of_days):
+#     return f"{num_of_days} days are {num_of_days * to_seconds} {name_of_unit}"
+#
+# my_var = days_to_units(20)
+# print(my_var)
+#
+# user_input = int(input("Hey user, enter a number of days and I will convert it to seconds:\n"))
+# calculated_value = days_to_units(user_input)
+# print(calculated_value)
+
+print('\nconditionals + boolean data type')
+# def days_to_units(num_of_days):
+#     print(type(num_of_days > 0))
+#     if num_of_days > 0:
+#         return f"{num_of_days} days are {num_of_days * to_seconds} seconds"
+#     elif num_of_days == 0:
+#         return "you entered 0"
+#
+# def validate_and_execute():
+#     if user_input.isdigit():
+#         user_input_number = int(user_input)
+#         calculated_value = days_to_units(user_input_number)
+#         print(calculated_value)
+#     else:
+#         print("Please enter a valid input.")
+#
+# user_input = (input("Hey user, enter a number of days and I will convert it to seconds:\n"))
+# validate_and_execute()
+
+print('\nnested if else')
+# def days_to_units(num_of_days):
+#     return f"{num_of_days} days are {num_of_days * to_seconds} seconds"
+#
+# def validate_and_execute():
+#     if user_input.isdigit():
+#         user_input_number = int(user_input)
+#         if user_input_number > 0:
+#             calculated_value = days_to_units(user_input_number)
+#             print(calculated_value)
+#         elif user_input_number == 0:
+#             print("you entered 0")
+#     else:
+#         return "your input is not a valid number. enter another one"
+#
+#
+# user_input = (input("Hey user, enter a number of days and I will convert it to seconds:\n"))
+# validate_and_execute()
+
+print('\nerror handling with try/except')
