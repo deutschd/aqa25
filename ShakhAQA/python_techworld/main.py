701d9333e1024e4e74ce12df1000c05df98de266
deutschd <ismailovshakhzod@gmail.com>
2026-04-27 17:54:49 +0500

    TechWorld with Nana - built-in functions + dictionary data types

--- ShakhAQA/python_techworld/main.py
+++ ShakhAQA/python_techworld/main.py
@@ -6,27 +6,27 @@ print('72%7 =', 72%7)
 print(f"10 days are {10 * 24 * 60} minutes")
 
 # variables
-to_seconds = 24 * 60 * 60
-name_of_unit = "seconds"
-print(f"10 days are {10 * to_seconds} {name_of_unit}")
-print(f"30 days are {30 * to_seconds} seconds")
-print(f"20 days are {20 * to_seconds} seconds")
-
-# functions + their parameters
-def days_to_units(num_of_days, custom_message):
-    print(f"{num_of_days} days are {num_of_days * to_seconds} {name_of_unit}")
-    print(custom_message)
-days_to_units(20, "Hello world")
-days_to_units(10, "Great!")
-
-# Scope
-def scope_check(num_of_days):
-    my_var = "variable inside function"
-    print(name_of_unit) # global variable
-    print(num_of_days) # local variable
-    print(my_var)
-
-scope_check(21)
+# to_seconds = 24 * 60 * 60
+# name_of_unit = "seconds"
+# print(f"10 days are {10 * to_seconds} {name_of_unit}")
+# print(f"30 days are {30 * to_seconds} seconds")
+# print(f"20 days are {20 * to_seconds} seconds")
+#
+# # functions + their parameters
+# def days_to_units(num_of_days, custom_message):
+#     print(f"{num_of_days} days are {num_of_days * to_seconds} {name_of_unit}")
+#     print(custom_message)
+# days_to_units(20, "Hello world")
+# days_to_units(10, "Great!")
+#
+# # Scope
+# def scope_check(num_of_days):
+#     my_var = "variable inside function"
+#     print(name_of_unit) # global variable
+#     print(num_of_days) # local variable
+#     print(my_var)
+#
+# scope_check(21)
 
 print('\naccepting user input')
 # def days_to_units(num_of_days):
@@ -150,15 +150,55 @@ print("\nlists & for loops")
 #         validate_and_execute()
 
 print("\nsets")
-def days_to_units(num_of_days):
-    return f"{num_of_days} days are {num_of_days * to_seconds} seconds"
+# def days_to_units(num_of_days):
+#     return f"{num_of_days} days are {num_of_days * to_seconds} seconds"
+#
+# def validate_and_execute():
+#     try:
+#         user_input_number = int(num_of_days_element)
+#         # we want to do conversion only for positive integers
+#         if user_input_number > 0:
+#             calculated_value = days_to_units(user_input_number)
+#             print(calculated_value)
+#         elif user_input_number == 0:
+#             print("you entered 0, enter a valid positive number")
+#         else:
+#             print("you entered a negative number, no conversion for you")
+#
+#     except ValueError:
+#         print("your input is not a valid number. enter another one")
+#
+# user_input = ""
+# while user_input != "exit":
+#     user_input = (input("Hey user, enter a number of days and I will convert it to seconds:\n"))
+#     list_of_days = user_input.split(", ")
+#
+#     print(list_of_days)
+#     print(set(list_of_days))
+#
+#     print(type(list_of_days))
+#     print(type(set(list_of_days)))
+#
+#     for num_of_days_element in set(list_of_days):
+#         validate_and_execute()
+
+
+print("\ndictionaries")
+
+def days_to_units(num_of_days, conversion_unit):
+    if conversion_unit == "hours":
+        return f"{num_of_days} days are {num_of_days * 24} hours"
+    elif conversion_unit == "minutes":
+        return f"{num_of_days} days are {num_of_days * 24 * 60} minutes"
+    else:
+        return "unsupported conversion"
 
 def validate_and_execute():
     try:
-        user_input_number = int(num_of_days_element)
+        user_input_number = int(days_and_unit_dictionary["days"])
         # we want to do conversion only for positive integers
         if user_input_number > 0:
-            calculated_value = days_to_units(user_input_number)
+            calculated_value = days_to_units(user_input_number, days_and_unit_dictionary["unit"])
             print(calculated_value)
         elif user_input_number == 0:
             print("you entered 0, enter a valid positive number")
@@ -170,15 +210,34 @@ def validate_and_execute():
 
 user_input = ""
 while user_input != "exit":
-    user_input = (input("Hey user, enter a number of days and I will convert it to seconds:\n"))
-    list_of_days = user_input.split(", ")
+    user_input = (input("Hey user, enter a number of days and conversion unit:\n"))
+    days_and_unit = user_input.split(":")
+    print(days_and_unit)
+    days_and_unit_dictionary = {"days": days_and_unit[0], "unit": days_and_unit[1]}
+    print(days_and_unit_dictionary)
+    print(type(days_and_unit_dictionary))
+    validate_and_execute()
 
-    print(list_of_days)
-    print(set(list_of_days))
+message = "enter some value"
+days = 20
+price = 9.99
+valid_number = True
+exit_input = False
+list_of_days = [20, 40, 20, 100]
+list_of_months = ["January", "February", "March", "April", "May"]
+set_of_days = {20, 45, 100} # without duplicates
+day_and_unit = {
+    "days": 20,
+    "unit": "days"
+}
 
-    print(type(list_of_days))
-    print(type(set(list_of_days)))
 
-    for num_of_days_element in set(list_of_days):
-        validate_and_execute()
-
+# my_list = ["20", "30", "100"]
+# print(my_list[2])
+#
+# my_dictionary = {
+#     "days": 20,
+#     "unit": "hours",
+#     "message": "hello world"
+# }
+# print(my_dictionary["message"])
